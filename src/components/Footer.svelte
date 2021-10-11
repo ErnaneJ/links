505c8508cb098684db63278d717bda44db470c5f
Ernane Ferreira <ernane.junior25@gmail.com>
2021-10-11 20:02:46 -0300

    Adcionando imagem e footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<footer>
+  <p>Desenvolvido com  <i class="fas fa-heart"></i> por Ernane Ferreira</p>
+</footer>
+
+<style>
+  footer{width: 100%;position:absolute; bottom: 0;margin-bottom: 10px;}
+  footer p {color: rgb(56, 56, 56); font-size: 12px;text-align: center;}
+  footer p i {color:#e22929;}
+</style>
